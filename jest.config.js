2fd1607d5eb510a2752167108574ae3d7cc19ee3
Nakshatrrra <nakshatratrivedi44@gmail.com>
2024-12-25 18:09:11 +0530

    Jest integration bug

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    rootDir: 'src',
+    testRegex: '.*\\.test\\.ts$', // Matches test files with .test.ts extension
+  };
+  
--- src/tests/book.test.ts
+++ src/tests/book.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import app from '../src/app';
+import app from '../app';
 
 describe('Library Management System - Add Books', () => {
     it('should add a new book to the library', async () => {
