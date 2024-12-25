cdc12a62527d5ee224a7c243084033c70963ad2f
Nakshatrrra <nakshatratrivedi44@gmail.com>
2024-12-25 18:48:43 +0530

    env added

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import bookRoutes from './routes/book.routes';
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 app.use('/api', bookRoutes);
 
-mongoose
-    .connect('mongodb+srv://naksh:naksh@cluster0.tcatx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.error('MongoDB connection error:', err));
+const mongoURI = process.env.MONGODB_URI;
 
+if (mongoURI) {
+    mongoose
+        .connect(mongoURI)
+        .then(() => console.log('MongoDB connected'))
+        .catch((err) => console.error('MongoDB connection error:', err));
+} else {
+    console.error('MongoDB URI is missing');
+}
 export default app;
